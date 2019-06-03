fdc30cd24e4fbc2cc4c73d8b4fa9dc83e5875adb
Paweł Urbańczyk <pawel.urbanczyk90@gmail.com>
2019-06-03 19:10:04 +0200

    added method which wait for load a modal but max 5sec

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -180,6 +180,17 @@ class FeatureContext extends RawMinkContext implements Context, SnippetAccepting
             " $('.modal:visible').length");
     }
 
-
-
+    /**
+     * Pauses the scenario until the user presses a key. Useful when debugging a scenario.
+     *
+     * @Then (I )break
+     */
+    public function iPutABreakpoint()
+    {
+        fwrite(STDOUT, "\033[s    \033[93m[Breakpoint] Press \033[1;93m[RETURN]\033[0;93m to continue...\033[0m");
+        while (fgets(STDIN, 1024) == '') {
+        }
+        fwrite(STDOUT, "\033[u");
+        return;
+    }
 }
